fca1b2a18ecafeec68b080768090b7f29338fe0c
charitra sharma <charitra889@gmail.com>
2020-12-29 12:58:28 -0700

    Add project files.

---
+++ HealthShark.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HealthShark.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ HealthShark.Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthShark.Utility
+{
+    public class SD
+    {
+        public const string Role_Admin = "Role_Admin";
+        public const string Role_Trainer = "Role_Trainer";
+        public const string Role_User = "Role_User";
+    }
+}
